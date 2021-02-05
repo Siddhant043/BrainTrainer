33ea2ad998ec91178a490791701a95a297852f87
Siddhant Mishra <siddhant48311@gmail.com>
2021-02-06 02:49:12 +0530

    Brain trainer app finished. App optimization done go button, timer, play again button features added.

--- app/src/main/java/com/example/braintrainer/MainActivity.java
+++ app/src/main/java/com/example/braintrainer/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.braintrainer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.solver.state.State;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
 import android.os.CountDownTimer;
@@ -14,47 +16,77 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     ArrayList<Integer> answersArr = new ArrayList<Integer>(); // creating answers holding array
-    Button goButton;
+    Button goButton, button0, button1, button2, button3, playAgainButton;
     int locationOfCorrectAnswer;
-    TextView result;
+    TextView resultTextView, scoreTextView, questionTextView, timerTextView;
+    ConstraintLayout gameLayout;
+    int score = 0;
+    int numberOfQuestions = 0;
 
-    public void start(View view){ // starting button
+    // starting button visibility
+    public void start(View view){ //starting go button functions
         goButton.setVisibility(View.INVISIBLE);
+        gameLayout.setVisibility(View.VISIBLE);
+        playAgain(scoreTextView); // playAgain functions requires a view to be passed, as we are not using view inside so it does not matters which one to pass.
     }
 
-    public void chooseAnswer(View view){ // function runs when a option is clicked
-        if(Integer.toString(locationOfCorrectAnswer).equals(view.getTag().toString())){
-            Log.i("Correct!", "You got it!");
-            result.setText("Correct!");
-        }else{
-            Log.i("Wrong!", "Better luck next time.");
-            result.setText("Wrong!");
+    // function runs when a option is clicked
+    public void chooseAnswer(View view){
+        if(Integer.toString(locationOfCorrectAnswer).equals(view.getTag().toString())) {
+            resultTextView.setText("Correct!");
+            score++;
         }
-
-
+        else
+            resultTextView.setText("Wrong!");
+        numberOfQuestions++ ;
+        scoreTextView.setText(Integer.toString(score) + "/" + Integer.toString(numberOfQuestions));
+        newQuestion();
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    // the function is used whenever the game is ends and at the beginning.
+    public void playAgain(View view){
+        score=0;
+        numberOfQuestions=0;
+        timerTextView.setText("30s");
+        scoreTextView.setText(Integer.toString(score) + "/" + Integer.toString(numberOfQuestions));
+        button0.setEnabled(true);
+        button1.setEnabled(true);
+        button2.setEnabled(true);
+        button3.setEnabled(true);
+        playAgainButton.setVisibility(View.INVISIBLE);
+        resultTextView.setText(null);
+        newQuestion();
+        new CountDownTimer(30100, 1000){  // timer function
 
-        goButton = (Button) findViewById(R.id.goButton);
-        TextView questionTextView = (TextView)findViewById(R.id.questionsTextView);
-        Button button0 = (Button)findViewById(R.id.button0);
-        Button button1 = (Button)findViewById(R.id.button1);
-        Button button2 = (Button)findViewById(R.id.button2);
-        Button button3 = (Button)findViewById(R.id.button3);
-        result = (TextView) findViewById(R.id.resultTextView);
+            @Override
+            public void onTick(long millisUntilFinished) {
+                int seconds = (int) (millisUntilFinished - 100) / 1000;
+                timerTextView.setText(seconds+"s");
+            }
 
-        result.setText(null); // Setting initial value of result so nothing appears on starting
+            @Override
+            public void onFinish() {
+                resultTextView.setText("Done!");
+                button0.setEnabled(false);
+                button1.setEnabled(false);
+                button2.setEnabled(false);
+                button3.setEnabled(false);
+                playAgainButton.setVisibility(View.VISIBLE);
 
+            }
+        }.start();
+    }
+
+    // function which shows a new question every time it is called.
+    public void newQuestion(){
         Random r = new Random(); //random object to pick random numbers
         int a = r.nextInt(21);
         int b = r.nextInt(21);
 
         questionTextView.setText(Integer.toString(a) + " + " + Integer.toString(b)); // setting question
         locationOfCorrectAnswer = r.nextInt(4); //deciding a location for right answer in array
+        answersArr.clear();
+
         for(int i=0; i<4; i++){ // loop to store values in array
             if(i == locationOfCorrectAnswer){ // if loop comes to the specified ans pos then enter the answer
                 answersArr.add(a+b);
@@ -71,5 +103,28 @@ public class MainActivity extends AppCompatActivity {
         button1.setText(Integer.toString(answersArr.get(1)));
         button2.setText(Integer.toString(answersArr.get(2)));
         button3.setText(Integer.toString(answersArr.get(3)));
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        goButton = (Button) findViewById(R.id.goButton);
+        questionTextView = (TextView)findViewById(R.id.questionsTextView);
+        button0 = (Button)findViewById(R.id.button0);
+        button1 = (Button)findViewById(R.id.button1);
+        button2 = (Button)findViewById(R.id.button2);
+        button3 = (Button)findViewById(R.id.button3);
+        resultTextView = (TextView) findViewById(R.id.resultTextView);
+        scoreTextView = (TextView) findViewById(R.id.scoreTextView);
+        timerTextView = (TextView) findViewById(R.id.timerTextView);
+        playAgainButton = (Button) findViewById(R.id.playAgainButton);
+        gameLayout = (ConstraintLayout) findViewById(R.id.gameLayout);
+
+        goButton.setVisibility(View.VISIBLE);
+        gameLayout.setVisibility(View.INVISIBLE);
+
+
+
     }
 }
